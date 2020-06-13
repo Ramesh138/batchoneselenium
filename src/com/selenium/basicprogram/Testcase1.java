6cf5c178e9ae690bb6451ad14ced24c76f017b86
Ramesh138 <ponneriramesh158@gmail.com>
2020-06-13 12:26:32 +0530

    u

--- src/com/selenium/basicprogram/Testcase1.java
+++ src/com/selenium/basicprogram/Testcase1.java
@@ -6,46 +6,57 @@ import org.testng.annotations.Test;
 
 public class Testcase1 extends sample {
 
+	/*
+	 * @Test
+	 * 
+	 * public void tc1() throws IOException {
+	 * 
+	 * System.out.println("username  :" + getdata("TC1", 0));
+	 * 
+	 * System.out.println("password  :" + getdata("TC1", 1));
+	 * 
+	 * System.out.println("fullname   :" + getdata("TC1", 2));
+	 * 
+	 * System.out.println("firstname  :" + getdata("TC1", 3));
+	 * 
+	 * }
+	 */
+
+	/*
+	 * @Test
+	 * 
+	 * public void tc2() throws IOException {
+	 * 
+	 * 
+	 * System.out.println("username  :"+getdata("TC2", 0));
+	 * 
+	 * System.out.println("password  :"+getdata("TC2", 1));
+	 * 
+	 * System.out.println("fullname   :"+getdata("TC2", 2));
+	 * 
+	 * System.out.println("firstname  :"+getdata("TC2",3 ));
+	 * 
+	 * 
+	 * for (int i = 0; i < 4; i++) { System.out.println("username  :"+getdata("TC2",
+	 * i)); }
+	 * 
+	 * 
+	 * 
+	 * for (int i = 0; i < 4; i++) { System.out.println("username  :"+getdata("TC2",
+	 * 0)); }
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 */
+
 	@Test
-	
-	public void tc1() throws IOException {
-		
-		
-		System.out.println("username  :"+getdata("TC1", 0));
-		
-		System.out.println("password  :"+getdata("TC1", 1));
-		
-		System.out.println("fullname   :"+getdata("TC1", 2));
-		
-		System.out.println("firstname  :"+getdata("TC1",3 ));
-		
-	
+	public void tc3() throws IOException {
+		System.out.println(" username :" + getPdata("username"));
+
+		System.out.println("password  :" + getPdata("password"));
+
 	}
-	
-	
-	@Test
-	
-	public void tc2() throws IOException {
-		
-		
-		System.out.println("username  :"+getdata("TC2", 0));
-		
-		System.out.println("password  :"+getdata("TC2", 1));
-		
-		System.out.println("fullname   :"+getdata("TC2", 2));
-		
-		System.out.println("firstname  :"+getdata("TC2",3 ));
-		
-		/*
-		 * for (int i = 0; i < 4; i++) { System.out.println("username  :"+getdata("TC2",
-		 * i)); }
-		 */
-		
-		
-		for (int i = 0; i < 4; i++) {
-			System.out.println("username  :"+getdata("TC2", 0));
-		}
-		
-	
-	}
+
 }
--- src/com/selenium/basicprogram/sample.java
+++ src/com/selenium/basicprogram/sample.java
@@ -3,24 +3,42 @@ package com.selenium.basicprogram;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class sample {
-	
-	public static String getdata(String testcasename,int cellnumber) throws IOException {
-		
-		File path= new File(".//driver//Testdata.xlsx");
-		
-		FileInputStream src=new FileInputStream(path);
-		
-		XSSFWorkbook wb= new XSSFWorkbook(src);
-		
-		XSSFSheet ws= wb.getSheet(testcasename);
-		
+
+	public static String getdata(String testcasename, int cellnumber) throws IOException {
+
+		File path = new File(".//driver//Testdata.xlsx");
+
+		FileInputStream src = new FileInputStream(path);
+
+		XSSFWorkbook wb = new XSSFWorkbook(src);
+
+		XSSFSheet ws = wb.getSheet(testcasename);
+
 		String data = ws.getRow(1).getCell(cellnumber).getStringCellValue();
+
+		return data;
+
+	}
+
+	public static String getPdata(String value) throws IOException {
+		
+		FileReader path= new FileReader(".//driver//data1.properties");
+		
+		Properties prop= new Properties();
+		
+		prop.load(path);
+		
+		String  data= prop.getProperty(value);
+		
+		System.out.println(data);
 		
 		return data;
 		
